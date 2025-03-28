55c7f20bc8588064a7ebc1d431e18a5ec3430d08
mc-stephen <stephenugo939@gmail.com>
2025-03-28 17:53:21 +0100

    Made some changes

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import { useState } from "react";
-import styles from "./style.module.css";
 import Image from "next/image";
+import styles from "./style.module.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function Portfolio() {
+  const maxShownProjects = 6;
+  const paginationLength = useRef(1);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [projects, setProjects] = useState<Record<string, string>[]>([]);
   const [selectedTab, setSelectedTab] = useState(PortfolioTab.all.valueOf());
+
+  function pagination(index: number) {
+    const nxtPageIndex = currentIndex + index;
+    console.log(nxtPageIndex);
+
+    if (nxtPageIndex >= 0 && nxtPageIndex < projects.length) {
+      setCurrentIndex(currentIndex + index);
+    }
+  }
+
+  useEffect(() => {
+    const allIsSelected = selectedTab === PortfolioTab.all;
+    const values = allIsSelected
+      ? Object.values(portfolio).flat() // Flatten all projects if "All" is selected
+      : portfolio[selectedTab] || []; // Get selected category
+
+    setProjects(values);
+  }, [selectedTab]);
 
   return (
     <div className={styles.portfolio}>
       <h2>Portfolio</h2>
 
+      {/*==================*/}
+      {/*  */}
+      {/*==================*/}
       <ul className={styles.tabs}>
         {Object.keys(portfolio).map((key) => {
           const isActive = selectedTab.valueOf() === key ? styles.active : "";
@@ -26,25 +51,54 @@ export default function Portfolio() {
         })}
       </ul>
 
+      {/*==================*/}
+      {/*  */}
+      {/*==================*/}
       <section className={styles.tabContent}>
-        {Object.entries(portfolio).map(([key, value]) => {
-          const isAll = key == PortfolioTab.all.valueOf();
-          const isSelected = selectedTab.valueOf() === key;
-          const projects = isAll ? value : isSelected ? value : [];
-          return projects.map((val, i) => (
-            <div key={i} className={styles.card}>
-              <div className={styles.imgCont}>
-                <Image src={val.image} alt={val.title} fill />
-                <div className={styles.frontLayer}>
-                  <i className="fa-regular fa-eye"></i>
+        {projects.map((val, i) => {
+          const min = maxShownProjects * currentIndex;
+          const max = maxShownProjects * (currentIndex + 1);
+          if (i >= min && i < max) {
+            return (
+              <div key={i} className={styles.card}>
+                <div className={styles.imgCont}>
+                  <Image src={val.image} alt={val.title} fill />
+                  <div className={styles.frontLayer}>
+                    <i className="fa-regular fa-eye"></i>
+                  </div>
                 </div>
+                <b className={styles.title}>{val.title}</b>
+                <span className={styles.category}>{val.category}</span>
               </div>
-              <b className={styles.title}>{val.title}</b>
-              <span className={styles.category}>{val.category}</span>
-            </div>
-          ));
+            );
+          }
         })}
       </section>
+
+      {/*==================*/}
+      {/*  */}
+      {/*==================*/}
+      {}
+      <ul className={styles.pagination}>
+        <li onClick={() => pagination(-1)}>
+          <i className="fa-solid fa-angle-left"></i>
+        </li>
+
+        <ul className={styles.numbering}>
+          {new Array(paginationLength.current).fill(0).map((val, i) => {
+            // const pagination = (projects.length / maxShownProjects).toFixed();
+            return (
+              <li key={i} onClick={() => setCurrentIndex(i + 1)}>
+                {i + 1}
+              </li>
+            );
+          })}
+        </ul>
+
+        <li onClick={() => pagination(+1)}>
+          <i className="fa-solid fa-angle-right"></i>
+        </li>
+      </ul>
     </div>
   );
 }
@@ -64,7 +118,7 @@ enum PortfolioTab {
 //======================
 //
 //======================
-const portfolio = {
+const portfolio: Record<string, Record<string, string>[]> = {
   [PortfolioTab.all]: [],
   [PortfolioTab.web]: [
     {
